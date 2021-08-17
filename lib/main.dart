57041473c6d2f5fcd9edd948bc82595c0591b8a7
FlutLab.io <>
2021-08-17 17:42:13 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'pages/home_page.dart';
 import 'pages/login_page.dart';
+import 'utils/routes.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() => runApp(MyApp());
