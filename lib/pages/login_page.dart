2d34a3974a473fa2ada124b72dfe8582195e89cc
FlutLab.io <>
2021-08-18 20:38:19 +0000

    Autosave

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -74,7 +74,7 @@ class _LoginPageState extends State<LoginPage> {
                               "Login",
                               style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 18),
                             ),
-                      decoration: BoxDecoration(color: Colors.lightBlue, borderRadius: BorderRadius.circular(changeButtton ? 20 : 8)),
+                      decoration: BoxDecoration(color: Colors.lightBlue, borderRadius: BorderRadius.circular(changeButtton ? 50 : 8)),
                     ),
                   )
                   // ElevatedButton(
